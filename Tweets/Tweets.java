b4df5e226cca164c9539f8c745ba08bdf56f5cc3
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-01-06 01:00:01 +0100

    Solution Updated

--- Tweets/Tweets.java
+++ Tweets/Tweets.java
@@ -5,12 +5,29 @@ import java.util.ArrayList;
 public class Tweets {
 
 	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		String user [] = sc.nextLine().split(" ");
+		ArrayList <String> opened = new ArrayList<>();
+
+		int N = Integer.parseInt(user[0]);
+		int K = Integer.parseInt(user[1]);
+
+		for (int i = 0; i < K; i++) {
+			String user_input = sc.nextLine();
+			if (user_input.equals("CLOSEALL")) {
+				opened.clear();
+			} else {
+				if (opened.contains(user_input)) {
+					opened.remove(user_input);
+				} else {
+					opened.add(user_input);
+				}
+			}
+			System.out.println(opened.size());
+		}
 
 
 	}
 
 }
-
-
-
 
